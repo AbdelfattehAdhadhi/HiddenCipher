3e67f75e499bce7f8d3f10d44fc60baf4aef764e
Abdelfatteh Adhadhi <46570582+AbdelfattehAdhadhi@users.noreply.github.com>
2024-08-31 16:01:00 +0100

    Implement GridManager for dynamic card layouts and scaling

---
+++ Assets/_GameAssets/Scripts/Grid/GridManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridManager : Singleton<GridManager>
+{
+    public RectTransform gridRectTransform;
+    public GridLayoutGroup gridLayoutGroup;
+
+    public GameObject cardPrefab;
+
+    [SerializeField] private int rows;
+    [SerializeField] private int cols;
+
+    private void Start()
+    {
+        CalculateGridCellSizeAndSpacing();
+        SetupGrid(rows, cols);
+    }
+
+    private void SetupGrid(int rowCount, int colCount)
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        int totalCards = rowCount * colCount;
+
+        for (int i = 0; i < totalCards; i++)
+        {
+            Instantiate(cardPrefab, transform);
+        }
+    }
+
+    private void CalculateGridCellSizeAndSpacing()
+    {
+        float gridWidth = gridRectTransform.rect.width;
+        float gridHeight = gridRectTransform.rect.height;
+        
+        float spacingRatio = 0.1f;
+       
+        float totalAvailableWidth = gridWidth * (1 - spacingRatio);
+        float totalAvailableHeight = gridHeight * (1 - spacingRatio);
+
+        
+        float cellSize = Mathf.Min(totalAvailableWidth / cols, totalAvailableHeight / rows);
+
+        
+        float spacingX = (gridWidth - (cellSize * cols)) / cols;
+        float spacingY = (gridHeight - (cellSize * rows)) / rows;
+
+        
+        gridLayoutGroup.cellSize = new Vector2(cellSize, cellSize);
+        gridLayoutGroup.spacing = new Vector2(spacingX, spacingY);
+        gridLayoutGroup.constraintCount = cols;
+    }
+}
